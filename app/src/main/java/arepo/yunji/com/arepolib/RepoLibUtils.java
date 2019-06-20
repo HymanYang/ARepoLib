67308e520de9e88bfa7f99d8b565e0e27fd04910
yangzhou <yangzhou@yunjiglobal.com>
2019-06-20 17:00:27 +0800

    ccc

---
+++ app/src/main/java/arepo/yunji/com/arepolib/RepoLibUtils.java
@@ -0,0 +1,18 @@
+package arepo.yunji.com.arepolib;
+
+/**
+ * @Description:
+ * @Author: XGY
+ * @Copyright: 浙江集商优选电子商务有限公司
+ * @CreateDate: 2019/6/20 16:52
+ * @Version: 1.0.0
+ */
+public class RepoLibUtils {
+
+
+    public static String getLibStr(){
+
+
+        return "I From Lib.";
+    }
+}
